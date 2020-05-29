14013706c85d48e492118d834f58a95292f91dc3
j.micah.manzanares <j.micah.manzanares@gmail.com>
2020-05-29 17:23:45 -0500

    n

--- BenBonkGmJmProject/Assets/Scripts/BehaviourMovement.cs
+++ BenBonkGmJmProject/Assets/Scripts/BehaviourMovement.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MovementBehaviour : MonoBehaviour
+public class BehaviourMovement : MonoBehaviour
 {
+
+    public Rigidbody rb;
+
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+
+    public Vector3 moveDir;
+
+    public GameObject player;
+
+    public CharacterController controller;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
-    {    
-        
+    {
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDir.y = jumpSpeed;
+            }
+
+            moveDir.y -= gravity * Time.deltaTime;
+            controller.Move(moveDir * Time.deltaTime * 2.5f);
+
+        }
     }
 }
